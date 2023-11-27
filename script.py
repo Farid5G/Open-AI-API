7efdbcfcfb230de2c1ff2f20a7fb79cf21668201
Farid Khan <fazalsh15@gmail.com>
2023-11-27 22:21:53 +0530

    Added Python File

---
+++ script.py
@@ -0,0 +1,19 @@
+from openai import OpenAI
+
+api_key = "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"
+client = OpenAI(api_key=api_key)
+
+while True:
+    prompt = input("Enter your prompt: ")
+
+    completion = client.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": prompt,
+            }
+        ],
+        model="gpt-3.5-turbo",
+    )
+
+    print(completion.choices[0].message.content)
